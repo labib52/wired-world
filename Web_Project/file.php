1df24ae46d52508f028b380bdbe307f1b943c2af
Amgademadd <amgad2108571@miuegypt.edu.eg>
2023-12-06 21:49:08 +0200

    database

---
+++ Web_Project/file.php
@@ -0,0 +1,27 @@
+<?php
+$conn=mysqli_connect("localhost","root","","electronics_e-commerce");
+if($conn===false){
+    die("error:could not connect.".mysqli_connect_error());
+}
+if(isset ($_POST[ 'submit'])){
+    // Taking all 5 values from the form data(input)
+    $first_name = $_POSTI ['fname'];
+    $last_name = $_POST['lname'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+
+$sq1 = "INSERT INTO feedback (firstname, lastname, email, subject) 
+VALUES ('$first_name','$last_name','$email','$subject')";
+
+if (mysqli_query ($conn, $sql)){
+echo
+"<h3›data stored in database successfully.".
+" Please browse your localhost php my admin"
+. " to view the updated data‹/h3>";
+} else{
+echo "ERROR: Hush! Sorry $sql. "
+. mysqli_error($conn);
+}
+}
+mysqli_close($conn);
+?>
